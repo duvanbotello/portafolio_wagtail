3aded1c57995375984bb8336ea704fb2c9825a3e
Duvan Botello <duvan.botello@megaterios.co>
2019-12-19 14:51:32 -0500

    initial commit

---
+++ portfolio/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '0p25ud+q4*@0rou*d805j@jakmqjw^djag5rrap*ebu^3ywm45'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
