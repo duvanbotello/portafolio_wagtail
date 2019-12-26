6ef5223a61f254b73049ffad1a94c5e4ca95f9f8
Duvan Botello <duvan.botello@megaterios.co>
2019-12-26 16:10:52 -0500

    Adding images for github

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -122,7 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-CO'
 
 TIME_ZONE = 'UTC'
 
